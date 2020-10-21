d3e3b2128b5600ec4d1454b9ec4a871b78e93c0b
binodrajpandey <binodraj.pandey@bebit.com>
2020-10-21 20:11:34 +0900

    add template

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,29 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Binod',
+        'title': 'Blog post 1',
+        'content': 'First Post Content',
+        'date_posted': 'August 28, 2020'
+    },
+    {
+        'author': 'Sandhya',
+        'title': 'Blog post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'August 29, 2020'
+    }
+]
+
+
 def home(request):
-    return HttpResponse('<h1> This is blog page</h1>')
+    # return HttpResponse('<h1> This is blog page</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
